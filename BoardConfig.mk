Tegra4/android_device_hp_bowser

zygote: Whitelist dev nodes used by zygote
Change-Id: I2c6cca1167efdaf1bdb39cb707d9cf7e566def15

BoardConfig.mk
@@ -14,6 +14,8 @@
 # limitations under the License.
 #
 
+TARGET_SPECIFIC_HEADER_PATH := device/hp/bowser/include
+
 # Architecture
 TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi

Tegra4/android_device_hp_phobos

BoardConfig.mk
@@ -17,6 +17,8 @@
 # This variable is set first, so it can be overridden
 # by BoardConfigVendor.mk
 
+TARGET_SPECIFIC_HEADER_PATH := device/hp/phobos/include
+
 # Architecture
 TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi